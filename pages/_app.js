e6d3e51ffdd42ec92b2a9a42ded2452163d69dc8
kadri4567 <maheshanandkadri@gmail.com>
2021-08-11 16:45:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+
+import '../styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/posts">
+                <a>Posts</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/authors">
+                <a>Authors</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
+}
